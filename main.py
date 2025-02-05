e2f3fd3860f35532d1bf39b2f32adcc036c44763
briar365 <briar.holloman.17@gmail.com>
2025-02-05 14:57:17 -0500

    Tick method

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from constants import *
 def main ():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock() 
+    dt = 0
     while pygame.get_init():
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                             return
         pygame.Surface.fill(screen, (0,0,1))
         pygame.display.flip()
+        Clock.tick(60)
+        dt = pygame.time.get_ticks()/1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
